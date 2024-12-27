8af6ac4aadebe6da8681d0c564f6221ce389f563
Daniel Gulotta <dgulotta@alum.mit.edu>
2024-12-26 21:24:07 -0700

    upgrade to clap 4

--- kleinian-cli/src/main.rs
+++ kleinian-cli/src/main.rs
@@ -1,47 +1,63 @@
-use clap::{value_t, App, Arg};
+use clap::{Arg, Command};
 use image::codecs::png::PngEncoder;
 use image::{ExtendedColorType, ImageEncoder};
 use kleinian::Cpx;
 use std::fs::File;
 
 fn main() {
-    let matches = App::new("kleinian")
-        .arg(Arg::with_name("width").required(true))
-        .arg(Arg::with_name("height").required(true))
+    let matches = Command::new("kleinian")
         .arg(
-            Arg::with_name("re1")
+            Arg::new("width")
                 .required(true)
-                .allow_hyphen_values(true),
+                .value_parser(clap::value_parser!(usize)),
         )
         .arg(
-            Arg::with_name("im1")
+            Arg::new("height")
                 .required(true)
-                .allow_hyphen_values(true),
+                .value_parser(clap::value_parser!(usize)),
         )
         .arg(
-            Arg::with_name("re2")
+            Arg::new("re1")
                 .required(true)
-                .allow_hyphen_values(true),
+                .value_parser(clap::value_parser!(f64))
+                .allow_negative_numbers(true),
         )
         .arg(
-            Arg::with_name("im2")
+            Arg::new("im1")
                 .required(true)
-                .allow_hyphen_values(true),
+                .value_parser(clap::value_parser!(f64))
+                .allow_negative_numbers(true),
         )
-        .arg(Arg::with_name("iters").required(true))
-        .arg(Arg::with_name("outfile").required(true))
+        .arg(
+            Arg::new("re2")
+                .required(true)
+                .value_parser(clap::value_parser!(f64))
+                .allow_negative_numbers(true),
+        )
+        .arg(
+            Arg::new("im2")
+                .required(true)
+                .value_parser(clap::value_parser!(f64))
+                .allow_negative_numbers(true),
+        )
+        .arg(
+            Arg::new("iters")
+                .required(true)
+                .value_parser(clap::value_parser!(usize)),
+        )
+        .arg(Arg::new("outfile").required(true))
         .get_matches();
-    let width = value_t!(matches.value_of("width"), usize).unwrap_or_else(|e| e.exit());
-    let height = value_t!(matches.value_of("height"), usize).unwrap_or_else(|e| e.exit());
-    let re1 = value_t!(matches.value_of("re1"), f64).unwrap_or_else(|e| e.exit());
-    let im1 = value_t!(matches.value_of("im1"), f64).unwrap_or_else(|e| e.exit());
-    let re2 = value_t!(matches.value_of("re2"), f64).unwrap_or_else(|e| e.exit());
-    let im2 = value_t!(matches.value_of("im2"), f64).unwrap_or_else(|e| e.exit());
-    let iters = value_t!(matches.value_of("iters"), usize).unwrap_or_else(|e| e.exit());
-    let filename = value_t!(matches.value_of("outfile"), String).unwrap_or_else(|e| e.exit());
+    let width: usize = *matches.get_one("width").unwrap();
+    let height: usize = *matches.get_one("height").unwrap();
+    let re1: f64 = *matches.get_one("re1").unwrap();
+    let im1: f64 = *matches.get_one("im1").unwrap();
+    let re2: f64 = *matches.get_one("re2").unwrap();
+    let im2: f64 = *matches.get_one("im2").unwrap();
+    let iters: usize = *matches.get_one("iters").unwrap();
+    let filename: &String = matches.get_one("outfile").unwrap();
     let p1 = Cpx::new(re1, im1);
     let p2 = Cpx::new(re2, im2);
-    let pts = kleinian::generate_points_from_traces(p1, p2, iters as usize);
+    let pts = kleinian::generate_points_from_traces(p1, p2, iters);
     let trans = kleinian::window::window_transform(&pts, width, height);
     let mut pixel_data = Vec::new();
     pixel_data.resize(width * height, 255);
@@ -52,6 +68,11 @@ fn main() {
     }
     let f = File::create(filename.as_str()).unwrap_or_else(|e| clap::Error::from(e).exit());
     PngEncoder::new(f)
-        .write_image(&pixel_data, width as u32, height as u32, ExtendedColorType::L8)
+        .write_image(
+            &pixel_data,
+            width as u32,
+            height as u32,
+            ExtendedColorType::L8,
+        )
         .unwrap();
 }
